b2e929010ec88f6b6d4372a2f414519575bb2fe5
aydam2728 <lesdmart@gmail.com>
2023-03-02 22:40:35 +0100

    add button event

--- src/main/java/com/example/champomatch/MyController.java
+++ src/main/java/com/example/champomatch/MyController.java
@@ -8,7 +8,7 @@ import java.awt.*;
 public class MyController {
 
 
-
+    @FXML
     public void likeEvent(ActionEvent event) {
         // Do something when the button is clicked
         System.out.println("You clicked me!");
