7d90e4885facdf11f8eb1ae10575ed6a9ae02bd5
Sylvell <adrien@alteirac.com>
2023-03-15 11:56:31 +0100

    ajout des attributs de RendezVous

--- src/main/java/com/example/champomatch/RendezVous.java
+++ src/main/java/com/example/champomatch/RendezVous.java
@@ -1,4 +1,37 @@
 package com.example.champomatch;
 
+import java.util.Date;
+
+enum State{
+    en_attente,
+    bien_passe,
+    mal_passe,
+
+}
 public class RendezVous {
+
+    public Single celib1;
+    public Single celib2;
+    public Date date;
+
+    public String lieu;
+
+    public State state = State.en_attente;
+
+
+    public RendezVous(Single s1, Single s2, Date date, String lieu){
+        this.celib1= s1;
+        this.celib2= s2;
+        this.date= date;
+        this.lieu= lieu;
+
+
+
+    }
+
+
+
+
+
+
 }
