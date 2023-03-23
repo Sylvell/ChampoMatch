f075d53d798e0f27546e05cc0e6e0b2f5447d6c8
aydam2728 <lesdmart@gmail.com>
2023-03-23 22:58:50 +0100

    add constructor and getters and setters for ArrayLists

--- src/main/java/com/example/champomatch/Single.java
+++ src/main/java/com/example/champomatch/Single.java
@@ -1,5 +1,4 @@
 package com.example.champomatch;
-
 import java.util.ArrayList;
 
 enum Gender {
@@ -23,6 +22,7 @@ enum Hobbies {
         }
 
 public class Single {
+    public Integer id = null;
     String name;
     int age;
     Gender gender;
@@ -30,13 +30,71 @@ public class Single {
     Gender preferedGender;
     String bio;
     String localisation;
-    ArrayList<Images> images;
+    ArrayList<Images> images = new ArrayList<Images>();
     Status status;
     int distance;
     int minimunAge;
     int maximunAge;
-    ArrayList<Single> liked;
-    ArrayList<Single> unliked;
-    ArrayList<Hobbies> Hobbies;
-    boolean isAlone;
+    ArrayList<Single> liked = new ArrayList<Single>();
+    ArrayList<Single> unliked = new ArrayList<Single>();
+    ArrayList<Single> candidates = new ArrayList<Single>();
+    ArrayList<Hobbies> hobbies = new ArrayList<Hobbies>();
+    boolean isAlone = true;
+
+    // Constructor
+
+
+    public Single(String name, int age, Gender gender, String pp, Gender preferedGender, String bio, String localisation, Status status, int distance, int minimunAge, int maximunAge) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.pp = pp;
+        this.preferedGender = preferedGender;
+        this.bio = bio;
+        this.localisation = localisation;
+        this.status = status;
+        this.distance = distance;
+        this.minimunAge = minimunAge;
+        this.maximunAge = maximunAge;
+    }
+
+    public ArrayList<Images> getImages() {
+        return images;
+    }
+
+    public void setImages(ArrayList<Images> images) {
+        this.images = images;
+    }
+
+    public ArrayList<Single> getLiked() {
+        return liked;
+    }
+
+    public void setLiked(ArrayList<Single> liked) {
+        this.liked = liked;
+    }
+
+    public ArrayList<Single> getUnliked() {
+        return unliked;
+    }
+
+    public void setUnliked(ArrayList<Single> unliked) {
+        this.unliked = unliked;
+    }
+
+    public ArrayList<Single> getCandidates() {
+        return candidates;
+    }
+
+    public void setCandidates(ArrayList<Single> candidates) {
+        this.candidates = candidates;
+    }
+
+    public ArrayList<Hobbies> getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(ArrayList<Hobbies> hobbies) {
+        this.hobbies = hobbies;
+    }
 }
