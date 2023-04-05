1e5b954838edd2f93062e7f1945c5a36dacf7fce
aydam2728 <lesdmart@gmail.com>
2023-04-05 11:51:33 +0200

    add simple code

--- src/main/java/com/example/champomatch/Images.java
+++ src/main/java/com/example/champomatch/Images.java
@@ -2,4 +2,17 @@ package com.example.champomatch;
 
 public class Images {
 
+    private String url;
+
+    public Images(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
