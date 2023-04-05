cac106eab0ea9efd2889f6e792258f27284d1b34
Gogack <franckgarciagonzalez@gmail.com>
2023-04-05 02:52:05 +0200

    update when your connection is a sucess

--- src/main/java/com/example/champomatch/Login.java
+++ src/main/java/com/example/champomatch/Login.java
@@ -14,7 +14,7 @@ public class Login extends Application {
         System.out.println(getClass());
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         stage.setTitle("User Login");
-        stage.setScene(new Scene(root, 1637, 1070));
+        stage.setScene(new Scene(root, 1637, 1060));
         stage.show();
     }
 
