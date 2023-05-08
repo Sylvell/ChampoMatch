db0889f61d55d1c5833997cf3c84bc87c122b891
aydam2728 <lesdmart@gmail.com>
2023-05-08 22:39:18 +0100

    test sound

--- src/main/java/com/example/champomatch/matchController.java
+++ src/main/java/com/example/champomatch/matchController.java
@@ -4,7 +4,10 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -44,12 +47,27 @@ public class matchController {
             if (match.getSingle() == null) {
                 this.nom2.setText("No match found");
                 this.percent.setText("0%");
+                // Create a Media object from a file path
+               /* Media sound = new Media(getClass().getResource("/emptyMatch.mp3").toString());
+
+                // Create a MediaPlayer object from the Media object
+                MediaPlayer mediaPlayer = new MediaPlayer(sound);
+
+                // Play the sound
+                mediaPlayer.play();*/
                 return;
             }else{
                 this.nom2.setText(match.getSingle().getFirstname() + " " + match.getSingle().getName());
                 this.percent.setText(match.getScore() + "%");
                 new ImageLoader(this.pp2, match.getSingle().getPp());
+                // Create a Media object from a file path
+                /*Media sound = new Media(getClass().getResource("/emptyMatch.mp3").toString());
 
+                // Create a MediaPlayer object from the Media object
+                MediaPlayer mediaPlayer = new MediaPlayer(sound);
+
+                // Play the sound
+                mediaPlayer.play();*/
             }
 
             new ImageLoader(this.pp1, this.single.getPp());
