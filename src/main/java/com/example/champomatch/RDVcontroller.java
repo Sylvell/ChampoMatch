986ec278d83448c92da50c9670d286c9a5bd6b4f
Gogack <franckgarciagonzalez@gmail.com>
2023-05-09 22:36:20 +0200

    Controller RDV + fxml + menu adjust

--- src/main/java/com/example/champomatch/RDVcontroller.java
+++ src/main/java/com/example/champomatch/RDVcontroller.java
@@ -1,5 +1,15 @@
 package com.example.champomatch;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Random;
@@ -8,6 +18,32 @@ public class RDVcontroller {
 
     private static ArrayList<RendezVous> rdv_list = new ArrayList<RendezVous>();
 
+    @FXML
+    private Button Retu;
+
+    private User userConnected;
+
+    public void setUserConnected(User userConnected) {
+        this.userConnected = userConnected;
+    }
+
+    @FXML
+    public void goback(ActionEvent event){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("menu.fxml"));
+            Parent root = loader.load();
+            menuController controller = loader.getController();
+            controller.setUser(this.userConnected);
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.centerOnScreen();
+            stage.setTitle("Menu");
+            stage.show();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
     public static Date datealea() {
         Random random = new Random();
